4d97262f2290506d883756912a8c1a0c04b10c7c
EvanKoo <EvanKoo@foxmail.com>
2017-07-14 19:31:41 +0800

    add related delete function

--- app/Activity.php
+++ app/Activity.php
@@ -9,4 +9,20 @@ class Activity extends Model
       protected $table = 'Activities';
       protected $fillable = array('moka','img','type','title','area','content','view','pass',
       'finish','price','start','end','local');
+
+      //删除时，同时删表Records的关联数据
+	  public static function boot()
+    {
+        parent::boot();
+
+        static::deleted(function ($model) {
+            $id = $model->id;Log::info('model moment`s id:'.$id);
+		$query = DB::table('Records')->where(['target'=>4,'target_id'=>$id])->first(); 	 	
+	      if($query){
+                  DB::table('Records')->where(['target'=>4,'target_id'=>$id])->delete();
+		}else{
+			return ;
+		}
+        });
+    }
 }
